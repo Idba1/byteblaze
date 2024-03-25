69c82b18c75e3cd785260d42d07af8899d0be3ce
Idba1 <moniraislamself@gmail.com>
2024-03-25 17:00:36 +0600

    set react router dom

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -1,8 +1,12 @@
+import Navbar from "../Components/Navbar";
 
 
 const Blogs = () => {
     return (
         <div>
+            <div className="h-16">
+                <Navbar></Navbar>
+            </div>
             <h1>This is a Blog page</h1>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import {RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import Blogs from './Pages/Blogs.jsx'
+import Bookmark from './Pages/Bookmark.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/blogs',
+    element: <Blogs></Blogs>,
+  },
+  {
+    path: '/bookmark',
+    element: <Bookmark></Bookmark>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </>,
 )
