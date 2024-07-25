56f6ae40d238ccdfab336a9717ca10fc40184480
Idba1 <moniraislamself@gmail.com>
2024-07-26 02:00:39 +0600

    show specific data in blogCard

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -1,17 +1,29 @@
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types'
 
-const BlogCard = () => {
+
+const BlogCard = ({ blog }) => {
+    console.log(blog);
+    const { cover_image, title, description, published_at, placeholderImg } = blog
     return (
         <div>
-            <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline ">
-                <img role="presentation" className="object-cover w-full rounded h-44 bg-gray-500" src="https://source.unsplash.com/random/480x360?1" />
+            <Link
+                to={`/blog/${blog.id}`}
+                className='max-w-sm mx-auto group hover:no-underline focus:no-underline '
+            >
+                <img role="presentation" className="className='object-cover w-full rounded h-44 " src={cover_image || placeholderImg} />
                 <div className="p-6 space-y-2">
-                    <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                    <span className="text-xs text-gray-400">January 21, 2021</span>
-                    <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
+                    <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+                    <span className="text-xs text-gray-400">{new Date(published_at).toLocaleDateString()}</span>
+                    <p>{description}</p>
                 </div>
-            </a>
+            </Link>
         </div>
     );
 };
+
+BlogCard.propTypes = {
+    blog: PropTypes.object.isRequired,
+  }
 
 export default BlogCard;
