31f166781d893d5fa8780dde772e04adacb83e7a
Idba1 <moniraislamself@gmail.com>
2024-07-26 02:22:13 +0600

    update card design and create dynamic route for blog details

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types'
-
+import placeholderImg from '../assets/404.jpg'
 
 const BlogCard = ({ blog }) => {
     console.log(blog);
-    const { cover_image, title, description, published_at, placeholderImg } = blog
+    const { cover_image, title, description, published_at} = blog
     return (
-        <div>
+        <div className="flex relative">
             <Link
                 to={`/blog/${blog.id}`}
-                className='max-w-sm mx-auto group hover:no-underline focus:no-underline '
+                className='max-w-sm mx-auto group transition border-2 p-2 hover:scale-105 border-opacity-30 border-primary hover:border-secondary  group hover:no-underline focus:no-underline'
             >
                 <img role="presentation" className="className='object-cover w-full rounded h-44 " src={cover_image || placeholderImg} />
                 <div className="p-6 space-y-2">
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Blogs from '../Pages/Blogs'
 import Home from '../Pages/Home'
 import ErrorPage from '../Pages/ErrorPage'
 import Bookmark from '../Pages/Bookmark'
+import Blog from '../Pages/Blog'
 
 
 export const router = createBrowserRouter([
@@ -21,7 +22,12 @@ export const router = createBrowserRouter([
         element: <Blogs></Blogs>,
         loader: () => fetch('https://dev.to/api/articles?per_page=20&top=7'),
       },
-     
+      {
+        path: '/blog/:id',
+        element: <Blog />,
+        loader: ({ params }) =>
+          fetch(`https://dev.to/api/articles/${params?.id}`),
+      },
       {
         path: '/bookmark',
         element: <Bookmark></Bookmark>
