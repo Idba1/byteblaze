1ba9ca98e2df2b068b344697d49b5da955e5ed98
Idba1 <moniraislamself@gmail.com>
2024-07-26 23:37:55 +0600

    show bookmark data

--- src/Pages/Bookmark.jsx
+++ src/Pages/Bookmark.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { getBlogs } from "../utils";
+import BlogCard from "../Components/BlogCard";
+
 const Bookmark = () => {
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
+        const savedBlogs = getBlogs()
+        setBlogs(savedBlogs)
+    }, [])
+
     return (
         <div>
-            <h1>This is a BookMark page</h1>
+            <div className='grid justify-center grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
+                {blogs.map(blog => (
+                    <BlogCard
+                        key={blog.id}
+                        blog={blog}
+                    ></BlogCard>
+                ))}
+            </div>
         </div>
     );
 };
